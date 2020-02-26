20be647391ae79d14e36e3544cde1ce7851c3367
Adrian Karwowski <53815823+Kierownick@users.noreply.github.com>
2020-02-26 18:31:23 +0100

    Operators Challege

---
+++ OperatorsChallenge/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double jeden = 20.00d;
+        double dwa = 80.00d;
+        double jedenDwa = (jeden + dwa) * 100.00d;
+        double moduloJedenDwa = (jedenDwa % 40.00d);
+
+        System.out.println(moduloJedenDwa);
+
+        boolean wynikModulo = moduloJedenDwa == 0d ? true : false;
+
+        if (!wynikModulo) {
+            System.out.println("Got some remainder");
+        }
+
+
+    }
+}
