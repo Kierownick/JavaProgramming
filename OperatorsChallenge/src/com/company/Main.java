8933d0de51f8815869200440c15de12c0a6187c0
Adrian Karwowski <53815823+Kierownick@users.noreply.github.com>
2020-02-26 19:00:33 +0100

    Operators Challenge

--- OperatorsChallenge/src/com/company/Main.java
+++ OperatorsChallenge/src/com/company/Main.java
@@ -8,15 +8,10 @@ public class Main {
         double dwa = 80.00d;
         double jedenDwa = (jeden + dwa) * 100.00d;
         double moduloJedenDwa = (jedenDwa % 40.00d);
-
-        System.out.println(moduloJedenDwa);
-
-        boolean wynikModulo = moduloJedenDwa == 0d ? true : false;
+        boolean wynikModulo = (moduloJedenDwa == 0) ? true : false;
 
         if (!wynikModulo) {
             System.out.println("Got some remainder");
         }
-
-
     }
 }
