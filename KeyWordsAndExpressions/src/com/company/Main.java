f6fb34e0402f0ffca9657de14bee98bfee82d545
Adrian Karwowski <53815823+Kierownick@users.noreply.github.com>
2020-02-26 19:35:23 +0100

    Programming MasterClass

---
+++ KeyWordsAndExpressions/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // a mile is equal to 1.609455 kilometres
+
+        double kilometres = (100 * 1.609455);
+
+        int highscore = 50;
+        if (highscore == 50) {
+            System.out.println("This is an expression");
+            highscore = highscore - 50;
+            System.out.println(highscore);
+
+        }
+
+    }
+}
---
+++ StatementsWhiteSpaceAndIndenting/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50; // statement = cała linia; expiression = wszystko po int i przed ;
+        myVariable++; // statement i expressions w jednym
+        myVariable--;
+        System.out.println("This is a test"); // To co w bracketach to expression
+
+        int a = 20;
+        System.out.println("Równa się = " +
+                a +
+                "kg");
+
+        int anotherVariable = 50;
+        myVariable--; // Lepiej tak nie pisać
+
+
+    }
+}
