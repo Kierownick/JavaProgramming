f6e2fa78c94fe4d192097150439f0b5dab616a7a
Adrian Karwowski <53815823+Kierownick@users.noreply.github.com>
2020-03-04 11:41:59 +0100

    Methods Challenge

--- MethodsChallenge/src/com/company/Main.java
+++ MethodsChallenge/src/com/company/Main.java
@@ -6,43 +6,52 @@ public class Main {
 
         // To jest podejście gościa, który rozwiązał zagadkę, nie podoba mi się za bardzo
         int position = calculateHighScorePosition(900232323);
-        displayHighScorePosition("Adrian", position);
+        displayHighScorePosition("Adrian", position, 900232323);
 
         // A to moje, bez alokowania pamięci zmienną.
         displayHighScorePosition(
                 "Karwowski",
                 calculateHighScorePosition(
-                        1500));
+                        1500), 1500);
 
         displayHighScorePosition(
-                "Kieras",
+                "Karwowski",
                 calculateHighScorePosition(
-                        900));
+                        900), 900);
 
         displayHighScorePosition(
-                "Adi",
+                "Karwowski",
                 calculateHighScorePosition(
-                        400));
+                        400), 400);
 
         displayHighScorePosition(
-                "Kierownick",
+                "Karwowski",
                 calculateHighScorePosition(
-                        50));
+                        50), 50);
+
     }
 
-    public static void displayHighScorePosition(String playersName, int position) {
-        System.out.println(playersName + " managed to get into position " + position + " on the high score table!");
+    public static void displayHighScorePosition(String playersName, int position, int playerScore) {
+        System.out.println(playersName + " managed to get into position " + position
+                + " on the high score table with "
+                + playerScore + " points!");
     }
 
     public static int calculateHighScorePosition(int playerScore) {
+
         if (playerScore >= 1000) {
             return 1;
-        } else if (playerScore >= 500 && playerScore < 1000) {
+        } else if (playerScore >= 500) {
             return 2;
-        } else if (playerScore >= 100 && playerScore < 500) {
+        } else if (playerScore >= 100) {
             return 3;
-        } else {
-            return 4;
+        }
+        return 4;
+
+//        int position = 4; //position 4 will be returned
+//        if(playerScore >= 1000) {
+//            position = 1;
+//    }
         }
     }
 }
